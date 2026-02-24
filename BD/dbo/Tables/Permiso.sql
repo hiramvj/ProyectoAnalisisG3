89b74f087c4588129c18813563684ea139825fd5
Andrescr25 <114454912+Andrescr25@users.noreply.github.com>
2026-02-24 12:49:25 -0600

    Clean up old DB files, debug logs, and fix scripts

--- BD/dbo/Tables/Permiso.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Permiso] (
-    [PermisoId]   INT            IDENTITY (1, 1) NOT NULL,
-    [Codigo]      NVARCHAR (80)  NOT NULL,
-    [Descripcion] NVARCHAR (200) NULL,
-    [Activo]      BIT            DEFAULT ((1)) NOT NULL,
-    PRIMARY KEY CLUSTERED ([PermisoId] ASC),
-    UNIQUE NONCLUSTERED ([Codigo] ASC)
-);
-
