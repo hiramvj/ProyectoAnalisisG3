f7ac0c42945852e7d35c8f929910c7f89bfbd62e
H!ram <hiramdj2006@gmail.com>
2026-03-24 12:04:54 -0600

    Informe financiero - desempeño economico (3)

---
+++ ProyectoTachi/Models/InformeFinancieroViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoTachi.Models
+{
+    public class InformeFinancieroViewModel
+    {
+        public decimal ValorTotalCosto { get; set; }
+        public decimal ValorTotalVenta { get; set; }
+        public decimal MargenGananciaEstimado { get; set; }
+        public List<DetalleFinancieroProducto> Productos { get; set; } = new();
+    }
+
+    public class DetalleFinancieroProducto
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Cantidad { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public decimal SubtotalVenta { get; set; }
+    }
+}
